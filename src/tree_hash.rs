477d5ce23d4f1bb109a2f62ee35bbfebfc83a846
Odinson <bpoulav@gmail.com>
2025-08-27 08:05:01 +0530

    Changed fn vec_tree_hash_root to use runtime N (#51)

--- src/tree_hash.rs
+++ src/tree_hash.rs
@@ -1,17 +1,15 @@
 use tree_hash::{Hash256, MerkleHasher, TreeHash, TreeHashType};
-use typenum::Unsigned;
 
 /// A helper function providing common functionality between the `TreeHash` implementations for
 /// `FixedVector` and `VariableList`.
-pub fn vec_tree_hash_root<T, N>(vec: &[T]) -> Hash256
+pub fn vec_tree_hash_root<T>(vec: &[T], max_leaves: usize) -> Hash256
 where
     T: TreeHash,
-    N: Unsigned,
 {
     match T::tree_hash_type() {
         TreeHashType::Basic => {
             let mut hasher = MerkleHasher::with_leaves(
-                (N::to_usize() + T::tree_hash_packing_factor() - 1) / T::tree_hash_packing_factor(),
+                (max_leaves + T::tree_hash_packing_factor() - 1) / T::tree_hash_packing_factor(),
             );
 
             for item in vec {
@@ -25,7 +23,7 @@ where
                 .expect("ssz_types variable vec should not have a remaining buffer")
         }
         TreeHashType::Container | TreeHashType::List | TreeHashType::Vector => {
-            let mut hasher = MerkleHasher::with_leaves(N::to_usize());
+            let mut hasher = MerkleHasher::with_leaves(max_leaves);
 
             for item in vec {
                 hasher
